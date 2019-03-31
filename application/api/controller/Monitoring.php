8ad93e02897e9ce73b8f48239d7fa6d468eacd89
xiaochaozi <29862285@qq.com>
2019-03-31 12:48:19 +0800

    1

--- application/api/controller/Monitoring.php
+++ application/api/controller/Monitoring.php
@@ -8,12 +8,12 @@ class Monitoring extends Base
 {
 
 	protected $tab_map = [
-		'szhs' 			=> ['和值','和值大小','和值大单双','和值单双','和值小单双','红大小','红单双','黑大小','黑单双','黑码','红码'],
-		'szzh' 			=> ['三不同号','三同号单选'],
-		'ezzh' 			=> ['二不同号','二同号复选','二同号单选'],
+		'szhs' 			  => ['和值','和值大小','和值大单双','和值单双','和值小单双','红大小','红单双','黑大小','黑单双','黑码','红码'],
+		'szzh' 			  => ['三不同号','三同号单选'],
+		'ezzh' 			  => ['二不同号','二同号复选','二同号单选'],
 		'slhtxsthtx' 	=> ['豹子','全顺'],
-		'szkhhm' 		=> ['5码黑','4码红','4码黑','杂','全顺','半顺','豹子','跨'],
-		'sj' 			=> ['三军'],
+		'bszkhhm' 		=> ['5码黑','4码红','4码黑','杂','全顺','半顺','豹子','跨'],
+		'sj' 			    => ['三军'],
 	];
 
 
@@ -46,9 +46,14 @@ class Monitoring extends Base
    		{
    			if ($game_key == 'jlk3') 
    			{
+
+
    				//$periods 	= get_k3_number() == $periods ? $periods : get_k3_number();
-   				$data  		= Db::table('order')->field('COUNT(`no`) AS num,play_key')->where('play_name','in',$this->tab_map[$tab])->where('part','=',$levelValue)->where('number','=',$periods)->group('play_key')->select();
-   				$return_data = array_column($data,'num','play_key');
+                if ($this->tab_map[$tab] ?? '') {
+
+           		   $data  		= Db::table('order')->field('COUNT(`no`) AS num,play_key')->where('play_name','in',$this->tab_map[$tab])->where('part','=',$levelValue)->where('number','=',$periods)->group('play_key')->select();
+           		   $return_data = array_column($data,'num','play_key');
+                }
    			}
    		}
         return json(['msg' => 'succeed','code' => 200, 'data' =>$return_data]);
