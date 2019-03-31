f26e3c13168fc17550b6a1e690d64bd74bd6c24f
hedianjie <1163486362@qq.com>
2019-03-31 17:55:34 +0800

    s

--- public/static/memberManagement/js/addInfo.js
+++ public/static/memberManagement/js/addInfo.js
@@ -14,9 +14,9 @@ var app = new Vue({
         name: '', // 会员名称
         pwd: '', // 密码
         confirm_pwd: '', // 确认密码
-        levelValue: [],
-        quick: 1000,
-        usable_quick: 2000,
+        levelValue: {},
+        quick: '',
+        usable_quick: 1,
         game_list: [],
     },
     methods: {
@@ -32,19 +32,52 @@ var app = new Vue({
                     this.usernameStatus = 1;
                 }
             })
+        },
+
+        submit: function(){
+            if(usernameStatus == 0){
+                alert('请检测账号是否重复！');
+                return;
+            }
+            var game = {};
+            for(var i  =0 ; i < this.game_list.length; i++) {
+                if(this.game_list[i].select){
+                    game[this.game_list[i].game_key] = this.game_list[i];
+                }
+            }
+            var data = {
+                agent_name: this.create_user_name,
+                user_num: this.username,
+                user_name: this.user_name,
+                password: this.pwd,
+                confirm_pwd: this.confirm_pwd,
+                quick_open_quote: this.quick,
+                part: this.levelValue,
+                game: game
+            };
+
+            utils.getAjax({
+
+                url: '/Member/addUser',
+                type: 'POST',
+                data: data,
+                success: function(result){
+                    history.back(1);
+                },
+                alert: true,
+            })
         }
     },
     mounted: function(){
         this.create_user_name = ENV.userInfo.user_name;
-        
+        this.usable_quick = ENV.userInfo.quick_open_quote;
 
         var level = {};
         for(var i = 0; i < ENV.userInfo.dish.length; i++) {
             level[ENV.userInfo.dish[i]] = 0
         }
         this.levelValue = level;
-
-
+        
         this.game_list = ENV.userInfo.game_list;
     },
 
