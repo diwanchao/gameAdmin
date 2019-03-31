0613decb238ad6bcc2f4e7adff4e76ba665f5bf0
xiaochaozi <29862285@qq.com>
2019-03-31 21:18:02 +0800

    1

--- application/index/controller/AgentManagement.php
+++ application/index/controller/AgentManagement.php
@@ -3,11 +3,30 @@ namespace app\index\controller;
 use app\index\controller\Base;
 
 
-class ChangePwd extends Base
+class AgentManagement extends Base
 {
     public function index()
     {
         return $this->fetch();
     }
-  
+    public function accountInfo()
+    {
+        return $this->fetch();
+    }
+    public function addInfo()
+    {
+        return $this->fetch();
+    }
+    public function editInfo()
+    {
+        return $this->fetch();
+    }
+    public function kindInfo()
+    {
+        return $this->fetch();
+    }
+    public function operationLog()
+    {
+        return $this->fetch();
+    }
 }
