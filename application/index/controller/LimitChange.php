692b17bedeb10cc18ee47b5769f9e29fcaedf621
xiaochaozi <29862285@qq.com>
2019-03-31 21:00:49 +0800

    1

---
+++ application/index/controller/LimitChange.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\index\controller;
+use app\index\controller\Base;
+
+
+class LimitChange extends Base
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+}
