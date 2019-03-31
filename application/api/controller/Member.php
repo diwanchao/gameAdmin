90db6ef8d658eaf9b5350ee91cb010a9a0ffdad2
xiaochaozi <29862285@qq.com>
2019-03-31 13:50:30 +0800

    1

--- application/api/controller/Member.php
+++ application/api/controller/Member.php
@@ -9,7 +9,7 @@ class Member extends Base
 	/**
 	 * 会员管理列表
 	 */
-	public function list()
+	public function userList()
 	{
 		$data =[
 			'total'=>10,
@@ -18,8 +18,20 @@ class Member extends Base
 				['agent_name'=>'dwc','user_number'=>'dwc123','user_name'=>'邸万超','part'=>'A,C,D','id'=>1,'quick_open_quote'=>'570','create_time'=>'02-26 15:27:10','login_count'=>'33','login_time'=>'03-20 20:12:34','status'=>1],
 			],
 		];
-
-
+        return json(['msg' => 'succeed','code' => 200, 'data' =>[]]);
+	}	
+	/**
+	 * 代理管理列表
+	 */
+	public function agentList()
+	{
+		$data =[
+			'total'=>10,
+			'data'=>[
+				['agent_name'=>'dwc','user_number'=>'dwc123','user_name'=>'邸万超','part'=>'A,C,D','id'=>1,'quick_open_quote'=>'570','create_time'=>'02-26 15:27:10','login_count'=>'33','login_time'=>'03-20 20:12:34','status'=>1],
+				['agent_name'=>'dwc','user_number'=>'dwc123','user_name'=>'邸万超','part'=>'A,C,D','id'=>1,'quick_open_quote'=>'570','create_time'=>'02-26 15:27:10','login_count'=>'33','login_time'=>'03-20 20:12:34','status'=>1],
+			],
+		];
         return json(['msg' => 'succeed','code' => 200, 'data' =>[]]);
 	}
 }
