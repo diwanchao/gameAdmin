5f6faf15f972e64ab8e6f5765a5678f20cd3c916
xiaochaozi <29862285@qq.com>
2019-03-31 21:07:23 +0800

    1

---
+++ application/index/controller/Login.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\index\controller;
+use app\index\controller\Base;
+
+
+class Login extends Base
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+  
+}
---
+++ application/index/controller/Result.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\index\controller;
+use app\index\controller\Base;
+
+
+class Result extends Base
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+  
+}
