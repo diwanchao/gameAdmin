42dafdc5e7f9bfa244575cddd36b832f3e5bc5a9
hedianjie <1163486362@qq.com>
2019-03-31 13:49:07 +0800

    s

--- public/static/memberManagement/js/index.js
+++ public/static/memberManagement/js/index.js
@@ -5,7 +5,7 @@ var app = new Vue({
             total: 100, // 总共多少条
             index: 1, // 当前第几页
             state: 1, // 停用启用 0->停用 1->启用
-            sort: 'createTime', // createTime->新增日期 account->名称 balance->信用额度 loginTime->最后登录
+            sort: 'create_time', // createTime->新增日期 account->名称 balance->信用额度 loginTime->最后登录
             user_name: '', // 用户姓名
         },
         
