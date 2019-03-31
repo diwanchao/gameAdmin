d2b980d0b6e6badb07f6fb43ed275d5b6f43f55e
xiaochaozi <29862285@qq.com>
2019-03-31 20:56:34 +0800

    1

---
+++ application/index/controller/AgentManagement.php
@@ -0,0 +1,32 @@
+<?php
+namespace app\index\controller;
+use app\index\controller\Base;
+
+
+class AgentManagement extends Base
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+    public function accountInfo()
+    {
+        return $this->fetch();
+    }
+    public function addInfo()
+    {
+        return $this->fetch();
+    }
+    public function editInfo()
+    {
+        return $this->fetch();
+    }   
+    public function kindInfo()
+    {
+        return $this->fetch();
+    }
+    public function operationLog()
+    {
+        return $this->fetch();
+    }    
+}
