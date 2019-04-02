c217c9e7b84491d3b5971bf069772949f717b13e
xiaochaozi <29862285@qq.com>
2019-04-02 23:24:13 +0800

    11

--- application/api/controller/Report.php
+++ application/api/controller/Report.php
@@ -24,9 +24,9 @@ class Report extends Base
     public function list()
     {
     	$data = [
-            'jlk3'=>['open_count'=>1,'not_open_count'=>1,'bet_amount'=>0,'sum_loss'=>1,'up_proportion'=>1,'self_proportion','down_proportion'=>2,'all_agent_break'=>1,'agent_break'=>20,'rebate'=>'返利','up_profit'=>100,'down_profit'=>1,'self_profit'=>9999],
-            'ssc'=>['open_count'=>1,'not_open_count'=>1,'bet_amount'=>0,'sum_loss'=>1,'up_proportion'=>1,'self_proportion','down_proportion'=>2,'all_agent_break'=>1,'agent_break'=>20,'rebate'=>'返利','up_profit'=>100,'down_profit'=>1,'self_profit'=>9999],
-            'down_total'=>['open_count'=>1,'not_open_count'=>1,'bet_amount'=>0,'sum_loss'=>1,'up_proportion'=>1,'self_proportion','down_proportion'=>2,'all_agent_break'=>1,'agent_break'=>20,'rebate'=>'返利','up_profit'=>100,'down_profit'=>1,'self_profit'=>9999],
+            'jlk3'=>['open_count'=>1,'not_open_count'=>1,'bet_amount'=>0,'sum_loss'=>1,'up_proportion'=>1,'self_proportion'=>0,'down_proportion'=>2,'all_agent_break'=>1,'agent_break'=>20,'rebate'=>'返利','up_profit'=>100,'down_profit'=>1,'self_profit'=>9999],
+            'ssc'=>['open_count'=>1,'not_open_count'=>1,'bet_amount'=>0,'sum_loss'=>1,'up_proportion'=>1,'self_proportion'=>0,'down_proportion'=>2,'all_agent_break'=>1,'agent_break'=>20,'rebate'=>'返利','up_profit'=>100,'down_profit'=>1,'self_profit'=>9999],
+            'down_total'=>['open_count'=>1,'not_open_count'=>1,'bet_amount'=>0,'sum_loss'=>1,'up_proportion'=>1,'self_proportion'=>0,'down_proportion'=>2,'all_agent_break'=>1,'agent_break'=>20,'rebate'=>'返利','up_profit'=>100,'down_profit'=>1,'self_profit'=>9999],
 
         ];
         return json(['msg' => 'succeed','code' => 200, 'data' =>$data]);
