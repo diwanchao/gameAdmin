9e11a20ca69b2064e5ebd95c403fbdfa088a2173
hedianjie <1163486362@qq.com>
2019-04-02 10:03:06 +0800

    s

---
+++ public/static/agentManagement/js/operationLog.js
@@ -0,0 +1,26 @@
+var id = utils.getURL(location.search, 'id');
+
+if(!id){
+    alert('请正确进入～');
+    history.back(1);
+}
+
+var app = new Vue({
+    el: '#main',
+    data: {
+        data: [],
+        id: id,
+    },
+
+    mounted: function(){
+        var _this = this;
+        utils.getAjax( {
+            url: '/api/Member/getOperationLog',
+            type: 'GET',
+            data: {id: this.id},
+            success: function(result){
+                _this.data = result;
+            }
+        });
+    }
+})
