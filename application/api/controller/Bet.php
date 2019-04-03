4226e81c005cb5b0bc291831ac4b0e6b88a806a9
xiaochaozi <29862285@qq.com>
2019-04-03 15:02:35 +0800

    1

---
+++ application/api/controller/Bet.php
@@ -0,0 +1,32 @@
+<?php
+namespace app\api\controller;
+use app\api\controller\Base;
+use \think\Request;
+use \think\Db;
+
+class Bet extends Base
+{
+
+    /**
+     * 构造方法
+     * @param Request $request Request对象
+     * @access public
+     */
+	public function __construct() 
+	{
+        parent::__construct();
+
+    }
+
+    /**
+     * 注单列表
+     */
+    public function list()
+    {
+    	$data = [
+            ['user_name'=>'dwc','user_num'=>'123','user_id'=>1,'time'=>date('Y-m-d H:i:s',time()),'game_type'=>'吉林快3','number'=>'108132','part'=>'A','game_num'=>'20190402018','play_name'=>'红马单双','content'=>'单','odds'=>2,'game_result'=>'123','money'=>12,'handsel'=>10,'break'=>1,'amount'=>1],
+            ['user_name'=>'dwc','user_num'=>'123','user_id'=>1,'time'=>date('Y-m-d H:i:s',time()),'game_type'=>'吉林快3','number'=>'108132','part'=>'A','game_num'=>'20190402018','play_name'=>'红马单双','content'=>'单','odds'=>2,'game_result'=>'123','money'=>12,'handsel'=>10,'break'=>1,'amount'=>1],
+        ];
+        return json(['msg' => 'succeed','code' => 200, 'data' =>$data]);
+    }
+}
