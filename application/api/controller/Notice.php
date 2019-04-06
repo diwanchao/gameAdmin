67cd9d881c78fd5b0df9b5759e981876880164b5
xiaochaozi <29862285@qq.com>
2019-04-06 09:34:00 +0800

    1

---
+++ application/api/controller/Notice.php
@@ -0,0 +1,52 @@
+<?php
+namespace app\api\controller;
+use app\api\controller\Base;
+use \think\Request;
+use \think\Db;
+
+class Notice extends Base
+{
+
+    /**
+     * 构造方法
+     * @param Request $request Request对象
+     * @access public
+     */
+	public function __construct() 
+	{
+        parent::__construct();
+
+    }
+
+    /**
+     * 公告列表
+     */
+    public function list()
+    {
+        $data = Db::name('nitoce')->select();
+        return json(['msg' => 'succeed','code' => 200, 'data' =>$data]);
+    }
+
+
+    public function add()
+    {
+        $content   = $this->request->param('content','');
+        $data = [
+            'content'=>$content,
+            'create_time'=>date('Y-m-d H:i:s',time()),
+        ];
+        Db::name('notice')->insert($data);
+
+        return json(['msg' => '添加成功','code' => 200, 'data' =>$data]);
+    }
+
+
+    public function edit()
+    {
+        $id   = $this->request->param('id',0);
+        $data = Db::name('nitoce')->where('id=?',[$id])->find();
+        return json(['msg' => 'succeed','code' => 200, 'data' =>$data]);
+
+    }
+
+}
