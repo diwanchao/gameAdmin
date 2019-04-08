78cdd92be073f60018290d3d8f075bf81f7a1277
xiaochaozi <29862285@qq.com>
2019-04-08 15:05:03 +0800

    1

--- application/api/controller/Information.php
+++ application/api/controller/Information.php
@@ -23,7 +23,7 @@ class Information extends Base
     public function list()
     {
       	$game_key  	= $this->request->param('game_key','');
-      	$user_id 	= 1;
+      	$user_id 	= $this->USER_ID ?? 0;
 
         $where  = 'user_id=? and game_key=?';
         $data   = Db::name('user_game_method')->where($where,[$user_id,$game_key])->select();
