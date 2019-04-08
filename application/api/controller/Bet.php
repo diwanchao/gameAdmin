c6f05bf2f42fd2f3a5eb4a6c5eb1a9417f7e4232
xiaochaozi <29862285@qq.com>
2019-04-08 23:51:50 +0800

    11

--- application/api/controller/Bet.php
+++ application/api/controller/Bet.php
@@ -60,7 +60,7 @@ class Bet extends Base
 
 
     	$data = [
-            'data'=>$res;
+            'data'=>$res,
 /*            'data'=>[
                 ['user_name'=>'dwc','user_num'=>'123','user_id'=>1,'time'=>date('Y-m-d H:i:s',time()),'game_type'=>'吉林快3','number'=>'108132','part'=>'A','game_num'=>'20190402018','play_name'=>'红马单双','content'=>'单','odds'=>2,'game_result'=>'123','money'=>12,'handsel'=>10,'break'=>1,'amount'=>1],
                 ['user_name'=>'dwc','user_num'=>'123','user_id'=>1,'time'=>date('Y-m-d H:i:s',time()),'game_type'=>'吉林快3','number'=>'108132','part'=>'A','game_num'=>'20190402018','play_name'=>'红马单双','content'=>'单','odds'=>2,'game_result'=>'123','money'=>12,'handsel'=>10,'break'=>1,'amount'=>1],
