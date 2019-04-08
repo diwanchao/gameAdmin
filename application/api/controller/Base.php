e2e4142497b00f631c91c6d9d3a73c448e884a7d
xiaochaozi <29862285@qq.com>
2019-04-08 10:34:43 +0800

    1

--- application/api/controller/Base.php
+++ application/api/controller/Base.php
@@ -7,6 +7,9 @@ use \think\Db;
 
 class Base extends Controller
 {    
+
+	public $USER_ID;
+
     /**
      * 构造方法
      * @param Request $request Request对象
@@ -15,6 +18,11 @@ class Base extends Controller
 	public function __construct() 
 	{
         parent::__construct();
-
+        if (Session::get('is_login')) 
+        {
+        	$this->USER_ID = Session::get('user_id');	
+        }else{
+        	return json(['msg' => 'fail','code' => 304, 'data' => []]);die();
+        }
     }
 }
