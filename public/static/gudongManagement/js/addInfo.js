0d1821b2d0e22a6606f11999ae57879ecaff9c33
hedianjie <1163486362@qq.com>
2019-04-09 16:15:12 +0800

    s

--- public/static/gudongManagement/js/addInfo.js
+++ public/static/gudongManagement/js/addInfo.js
@@ -60,7 +60,7 @@ var app = new Vue({
 
             utils.getAjax({
 
-                url: '/api/menber/addShareholder',
+                url: '/api/member/addShareholder',
                 type: 'POST',
                 data: data,
                 success: function(result){
