8cd557c6c9414231501ead9d44e19ad40ed9ea84
xiaochaozi <29862285@qq.com>
2019-04-09 10:41:14 +0800

    1

--- application/api/controller/Bet.php
+++ application/api/controller/Bet.php
@@ -27,6 +27,7 @@ class Bet extends Base
         $game_key   = $this->request->param('game_key',''); 
         $time       = $this->request->param('time',date('Y-m-d',time())); 
         $status     = $this->request->param('status',''); 
+        $page       = $this->request->param('index',1); 
 
         $where[] = ['time','=',$time];
         if ($game_key) 
@@ -36,40 +37,39 @@ class Bet extends Base
         if ($status) 
             $where[] = ['status','=',$status];
 
-        $sql = "SELECT
-                user_name,
-                user_number as user_num,
-                o.user_id,
-                o.time,
-                o.game_key,
-                o. NO AS number,
-                o.part,
-                o.number AS game_num,
-                play_name,
-                content,
-                odds,
-                game_result,
-                money,
-                handsel,
-                break
-            FROM
-                `order` o
-            LEFT JOIN menber m ON o.user_id = m.id";
-            //echo $sql;die();
-        $res = Db::query($sql);
+        $money=$handsel=$break=$amount=0;
+
+
+        $total  = Db::name('order')->alias('o')->leftJoin('menber m','o.user_id = m.id')->value('count(1)');
+        $res    = Db::name('order')
+            ->alias('o')
+            ->field('user_name,user_number as user_num,o.user_id,o.time,o.game_key,o. NO AS number,o.part,o.number AS game_num,play_name,content,odds,game_result,money,handsel,break')
+            ->leftJoin('menber m','o.user_id = m.id')
+            ->page($page,10)
+            ->select();
+        if ($res) {
+            foreach ($res as $key => $value) 
+            {
+                $money      += $value['money'] ?? 0;
+                $handsel    += $value['handsel'] ?? 0;
+                $break      += $value['break'] ?? 0;
+                $amount     += $value['amount'] ?? 0;
+            }
+        }
+
 
 
     	$data = [
-            'data'=>$res,
+            'data'      => $res,
 /*            'data'=>[
                 ['user_name'=>'dwc','user_num'=>'123','user_id'=>1,'time'=>date('Y-m-d H:i:s',time()),'game_type'=>'吉林快3','number'=>'108132','part'=>'A','game_num'=>'20190402018','play_name'=>'红马单双','content'=>'单','odds'=>2,'game_result'=>'123','money'=>12,'handsel'=>10,'break'=>1,'amount'=>1],
                 ['user_name'=>'dwc','user_num'=>'123','user_id'=>1,'time'=>date('Y-m-d H:i:s',time()),'game_type'=>'吉林快3','number'=>'108132','part'=>'A','game_num'=>'20190402018','play_name'=>'红马单双','content'=>'单','odds'=>2,'game_result'=>'123','money'=>12,'handsel'=>10,'break'=>1,'amount'=>1],
             ],*/
-            'total'=>10,
-            'money'=>10,
-            'handsel'=>10,
-            'break'=>10,
-            'amount'=>2,
+            'total'     => $total ?? 0,
+            'money'     => sprintf("%.2f",$money),
+            'handsel'   => sprintf("%.2f",$handsel),
+            'break'     => sprintf("%.2f",$break),
+            'amount'    => sprintf("%.2f",$amount),
         ];
         return json(['msg' => 'succeed','code' => 200, 'data' =>$data]);
     }
