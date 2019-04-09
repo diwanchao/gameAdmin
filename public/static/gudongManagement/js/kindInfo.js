9cca25e7deb78497e0be86afb534c4075b4eda0b
hedianjie <1163486362@qq.com>
2019-04-09 15:12:57 +0800

    s

---
+++ public/static/gudongManagement/js/kindInfo.js
@@ -0,0 +1,75 @@
+var id = utils.getURL(location.search, 'id');
+var name = utils.getURL(location.search, 'name');
+
+if(!id){
+    alert('请正确进入～');
+    history.back(1);
+}
+
+var app = new Vue({
+    el: '#main',
+    data: {
+        id: id,
+        create_user_name: '',
+        name: name, // 会员名称
+        game_list: [],
+        levelValue: {},
+    },
+
+    methods: {
+
+        submit: function(){
+            var game = {};
+            for(var i  =0 ; i < this.game_list.length; i++) {
+                if(this.game_list[i].select){
+                    game[this.game_list[i].game_key] = true;
+                }
+            }
+            var data = {
+                part: this.levelValue,
+                game: game,
+                id: this.id
+            };
+
+            utils.getAjax({
+
+                url: '/api/Member/setAgentGameInfo',
+                type: 'POST',
+                data: data,
+                success: function(result){
+                    history.back(1);
+                },
+                alert: true,
+            })
+        }
+
+    },
+
+    mounted() {
+        var _this = this;
+        var game_list = ENV.userInfo.game_list;
+        var part_list = ENV.userInfo.dish;
+        this.create_user_name = ENV.userInfo.user_name;
+        utils.getAjax({
+            url: '/api/Member/addAgentGameInfo',
+            type: 'GET',
+            data: {id: this.id},
+            success: function(result){
+                for(var i = 0 ; i < game_list.length; i++) {
+                    var key = game_list[i].game_key;
+                    if(result.game[key]){
+                        game_list[i].select = true;
+                    }
+                }
+                _this.game_list = game_list;
+
+                var level = {};
+                for(var i = 0 ; i < part_list.length; i++) {
+                    level[part_list[i]] = result.part[part_list[i]];
+                }
+
+                _this.levelValue = level;
+            }
+        })
+    },
+});
