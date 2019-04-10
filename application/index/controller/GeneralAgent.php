e702bbd148ce8970900affdafec92b65b3dd5fcf
xiaochaozi <29862285@qq.com>
2019-04-10 21:12:33 +0800

    1

--- application/index/controller/GeneralAgent.php
+++ application/index/controller/GeneralAgent.php
@@ -7,22 +7,22 @@ class GeneralAgent extends Base
 {
     public function index()
     {
-        return $this->fetch('zongdaili_management/index');
+        return $this->fetch('zondaili_management/index');
     }
     public function accountInfo(){
-        return $this->fetch('zongdaili_management/account_info');
+        return $this->fetch('zondaili_management/account_info');
     }
     public function kindInfo(){
-        return $this->fetch('zongdaili_management/kind_info');
+        return $this->fetch('zondaili_management/kind_info');
     }     
     public function addInfo(){
-        return $this->fetch('zongdaili_management/add_info');
+        return $this->fetch('zondaili_management/add_info');
     }  
     public function editInfo(){
-        return $this->fetch('zongdaili_management/edit_info');
+        return $this->fetch('zondaili_management/edit_info');
     }     
     public function operationLog(){
-        return $this->fetch('zongdaili_management/operation_log');
+        return $this->fetch('zondaili_management/operation_log');
     } 
 
 
