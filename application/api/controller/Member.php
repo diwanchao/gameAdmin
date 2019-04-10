8aaba41d9d2213812bed968c3d61d861c8a8d623
xiaochaozi <29862285@qq.com>
2019-04-10 22:15:18 +0800

    1

--- application/api/controller/Member.php
+++ application/api/controller/Member.php
@@ -72,9 +72,9 @@ class Member extends Base
 
 		$data = Db::name('menber')
 		->alias('m1')
-		->field('m1.id,m2.user_name AS general_name,m1.user_name,m1.user_number,m1.blance AS quick_open_quote,m1.create_time,m1.login_time,m1.`status`,m1.bet_status')
+		->field('m1.id,m2.user_name AS general_name,m1.user_name,m1.user_number,IFNULL(m3.count_user,0) as count_user,m1.blance AS quick_open_quote,m1.create_time,m1.login_time,m1.`status`,m1.bet_status')
 		->leftJoin('menber m2','m1.parent_id=m2.id')
-//		->leftJoin([$subsql=> 'm3'],'m1.id=m3.parent_id')
+		->leftJoin([$subsql=> 'm3'],'m1.id=m3.parent_id')
 		->where($where)
 		->order('m1.'.$order, 'desc')
 		->fetchSql(true)
@@ -99,7 +99,9 @@ class Member extends Base
 		$status 	= $this->request->param('status',1);
 		$user_name 	= $this->request->param('user_name','');
 		$order 		= $this->request->param('sort','create_time');
+		$index 		= $this->request->param('index',0);
 
+		var_dump($index);die();
 		$where[] = ['m1.role_id','=',3];
 		$where[] = ['m1.status','=',$status];
 		if ($user_name) 
