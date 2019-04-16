6df36ba3cd0d59de1e00ce447b0b3b08a9b3cffe
xiaochaozi <29862285@qq.com>
2019-04-16 21:23:35 +0800

    1

--- application/api/controller/Report.php
+++ application/api/controller/Report.php
@@ -19,7 +19,7 @@ class Report extends Base
     }
 
     /**
-     * 获取第一条公告
+     * 获取列表
      */
     public function list()
     {
@@ -28,6 +28,17 @@ class Report extends Base
             'ssc'=>['open_count'=>1,'not_open_count'=>1,'bet_amount'=>0,'sum_loss'=>1,'up_proportion'=>1,'self_proportion'=>0,'down_proportion'=>2,'all_agent_break'=>1,'agent_break'=>20,'rebate'=>'返利','up_profit'=>100,'down_profit'=>1,'self_profit'=>9999],
             'down_total'=>['open_count'=>1,'not_open_count'=>1,'bet_amount'=>0,'sum_loss'=>1,'up_proportion'=>1,'self_proportion'=>0,'down_proportion'=>2,'all_agent_break'=>1,'agent_break'=>20,'rebate'=>'返利','up_profit'=>100,'down_profit'=>1,'self_profit'=>9999],
 
+        ];
+        return json(['msg' => 'succeed','code' => 200, 'data' =>$data]);
+    }
+    /**
+     * 头部统计
+     */
+    public function head()
+    {
+        $data = [
+            ['down_name'=>'dwc','up_num'=>1000,'self_num'=>1000,'down_num'=>1000];
+            ['down_name'=>'hdj','up_num'=>1000,'self_num'=>1000,'down_num'=>1000];
         ];
         return json(['msg' => 'succeed','code' => 200, 'data' =>$data]);
     }
