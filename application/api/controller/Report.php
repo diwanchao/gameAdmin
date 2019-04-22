0aa9c3aba826c40901f318d9ec3c205688356e1c
xiaochaozi <29862285@qq.com>
2019-04-22 17:16:15 +0800

    1

--- application/api/controller/Report.php
+++ application/api/controller/Report.php
@@ -60,11 +60,62 @@ class Report extends Base
                 $value['self_profit']       = $value['down_proportion'] - $value['up_proportion'];
                 $data[$value['game_key']][] = $value;
             }
+        }elseif ($user_info['role_id'] == 3) {
+            $where[] = [$self.'_id','=',$user_id];
+            $where[] = ["order.game_key",'in',$game_key];
+            $res = Db::name('order')
+                ->field("SUM(break) AS break,IFNULL(user_proportion,0) AS user_proportion,IFNULL(parent_proportion,100) AS parent_proportion,user_name,{$down}_id as user_id,user_number,user_name AS down_name,order.game_key,IF(`order`.`status`=1,COUNT(1),0) AS open_count,IF(`order`.`status`=0,COUNT(1),0) AS not_open_count,SUM(money) AS bet_amount,SUM(get) AS sum_loss,SUM({$self}_earn) AS self_proportion,SUM(2_earn+1_earn) AS down_proportion,SUM({$up}_earn) as up_proportion")
+                ->leftJoin('menber',"`order`.`{$down}_id`=menber.id")
+                ->where($where)
+                ->whereBetweenTime("time", $start_time, $end_time)
+                ->leftJoin('break_log',"`order`.`3_id` = break_log.user_id AND `order`.game_key = break_log.game_key")
+                ->group("{$down}_id,`order`.`game_key`")
+                ->fetchSql(0)
+                ->select();
+            foreach ($res as $key => $value) {
+                $value['sum_loss']          = $value['sum_loss'] > 0 ? -1*$value['sum_loss'] : abs($value['sum_loss']);
+                $value['self_proportion']   = $value['self_proportion'] > 0 ? -1*$value['self_proportion'] : abs($value['self_proportion']);
+                $value['up_proportion']     = $value['up_proportion'] > 0 ? -1*$value['up_proportion'] : abs($value['up_proportion']);
+                $value['down_proportion']   = $value['down_proportion'] > 0 ? -1*$value['down_proportion'] : abs($value['down_proportion']);
+                $value['self_back']         = $value['break']*$value['user_proportion']*0.01;
+                $value['up_back']           = $value['break']*$value['parent_proportion']*0.01;
+                $value['down_back']         = 0;
+                $value['rebate']            = 0;
+                $value['up_profit']         = $value['up_proportion'] + $value['down_back'];
+                $value['down_profit']       = $value['sum_loss'] + $value['down_back'];
+                $value['self_profit']       = $value['down_proportion'] - $value['up_proportion'];
+                $data[$value['game_key']][] = $value;   
+            }
+        }elseif ($user_info['role_id'] == 2) {
+            $where[] = [$self.'_id','=',$user_id];
+            $where[] = ["order.game_key",'in',$game_key];
+            $res = Db::name('order')
+                ->field("SUM(break) AS break,user_name,{$down}_id as user_id,user_number,user_name AS down_name,game_key,IF(`order`.`status`=1,COUNT(1),0) AS open_count,IF(`order`.`status`=0,COUNT(1),0) AS not_open_count,SUM(money) AS bet_amount,SUM(get) AS sum_loss,SUM({$self}_earn) AS self_proportion,SUM(1_earn) AS down_proportion,SUM(3_earn+4_earn) as up_proportion")
+                ->leftJoin('menber',"`order`.`{$down}_id`=menber.id")
+                ->where($where)
+                ->whereBetweenTime("time", $start_time, $end_time)
+                ->group("{$down}_id,`order`.`game_key`")
+                ->select();
+
+            foreach ($res as $key => $value) {
+                $value['sum_loss']          = $value['sum_loss'] > 0 ? -1*$value['sum_loss'] : abs($value['sum_loss']);
+                $value['self_proportion']   = $value['self_proportion'] > 0 ? -1*$value['self_proportion'] : abs($value['self_proportion']);
+                $value['up_proportion']     = $value['up_proportion'] > 0 ? -1*$value['up_proportion'] : abs($value['up_proportion']);
+                $value['down_proportion']   = $value['down_proportion'] > 0 ? -1*$value['down_proportion'] : abs($value['down_proportion']);
+                $value['self_back']         = 0;
+                $value['up_back']           = $value['break'];
+                $value['down_back']         = 0;
+                $value['rebate']            = 0;
+                $value['up_profit']         = $value['up_proportion'] + $value['down_back'];
+                $value['down_profit']       = $value['sum_loss'] + $value['down_back'];
+                $value['self_profit']       = $value['down_proportion'] - $value['up_proportion'];
+                $data[$value['game_key']][] = $value;   
+            }
         }elseif ($user_info['role_id'] == 1) {
             $where[] = [$self.'_id','=',$user_id];
             $where[] = ['game_key','in',$game_key];
             $res = Db::name('order')
-                ->field("user_name,user_id,user_number,user_name AS down_name,game_key,IF(`order`.`status`=1,COUNT(1),0) AS open_count,IF(`order`.`status`=0,COUNT(1),0) AS not_open_count,SUM(money) AS bet_amount,SUM(get) AS sum_loss,SUM({$self}_earn) AS self_proportion,SUM(get) AS down_proportion,SUM({$up}_earn) as up_proportion")
+                ->field("SUM(break) AS break,user_name,user_id,user_number,user_name AS down_name,game_key,IF(`order`.`status`=1,COUNT(1),0) AS open_count,IF(`order`.`status`=0,COUNT(1),0) AS not_open_count,SUM(money) AS bet_amount,SUM(get) AS sum_loss,SUM({$self}_earn) AS self_proportion,SUM(get) AS down_proportion,SUM(2_earn+3_earn+4_earn) as up_proportion")
                 ->leftJoin('menber',"`order`.`user_id`=menber.id")
                 ->where($where)
                 ->whereBetweenTime("time", $start_time, $end_time)
@@ -76,6 +127,7 @@ class Report extends Base
                 $value['self_proportion']   = $value['self_proportion'] > 0 ? -1*$value['self_proportion'] : abs($value['self_proportion']);
                 $value['up_proportion']     = $value['up_proportion'] > 0 ? -1*$value['up_proportion'] : abs($value['up_proportion']);
                 //$value['down_proportion']   = $value['down_proportion'] > 0 ? -1*$value['down_proportion'] : abs($value['down_proportion']);
+                $value['up_back']           = $value['break'];
                 $value['self_back']         = 0;
                 $value['down_back']         = 0;
                 $value['rebate']            = 0;
@@ -84,7 +136,7 @@ class Report extends Base
                 $value['self_profit']       = $value['down_proportion'] - $value['up_proportion'];
                 $data[$value['game_key']][] = $value;
             }
-        }else{
+        }/*else{
             $where[] = [$self.'_id','=',$user_id];
             $where[] = ['game_key','in',$game_key];
             $res = Db::name('order')
@@ -108,7 +160,7 @@ class Report extends Base
                 $value['self_profit']       = $value['down_proportion'] - $value['up_proportion'];
                 $data[$value['game_key']][] = $value;   
             }
-        }
+        }*/
 
         return json(['msg' => 'succeed','code' => 200, 'data' =>$data]);
     }
@@ -133,7 +185,7 @@ class Report extends Base
             $where[] = ['game_key','in',$game_key];
 
             $data = Db::name('order')
-                ->field("SUM({2_earn+3_earn+4_earn) as up_num,SUM({$self}_earn) AS self_num,SUM(get) AS down_num,user_name AS down_name")
+                ->field("SUM(2_earn+3_earn+4_earn) as up_num,SUM({$self}_earn) AS self_num,SUM(get) AS down_num,user_name AS down_name")
                 ->leftJoin('menber'," `order`.`user_id` = menber.id")
                 ->group("user_id")
                 ->whereBetweenTime("time", $start_time, $end_time)
@@ -151,7 +203,7 @@ class Report extends Base
             $where[] = ['game_key','in',$game_key];
 
             $data = Db::name('order')
-                ->field("SUM({3_earn+4_earn) as up_num,SUM({$self}_earn) AS self_num,SUM(1_earn) AS down_num,user_name AS down_name")
+                ->field("SUM(3_earn+4_earn) as up_num,SUM({$self}_earn) AS self_num,SUM(1_earn) AS down_num,user_name AS down_name")
                 ->leftJoin('menber'," `order`.`{$down}_id` = menber.id")
                 ->group("{$down}_id")
                 ->whereBetweenTime("time", $start_time, $end_time)
@@ -169,7 +221,7 @@ class Report extends Base
             $where[] = ['game_key','in',$game_key];
 
             $data = Db::name('order')
-                ->field("SUM({4_earn) as up_num,SUM({$self}_earn) AS self_num,SUM(1_earn+2_earn) AS down_num,user_name AS down_name")
+                ->field("SUM(4_earn) as up_num,SUM({$self}_earn) AS self_num,SUM(1_earn+2_earn) AS down_num,user_name AS down_name")
                 ->leftJoin('menber'," `order`.`{$down}_id` = menber.id")
                 ->group("{$down}_id")
                 ->whereBetweenTime("time", $start_time, $end_time)
